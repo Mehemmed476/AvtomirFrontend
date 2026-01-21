b31e19589d70b936609164d5a5981140ceb46a07
Mehemmed476 <mehemmedxelilzade11@gmail.com>
2026-01-21 21:29:45 +0400

    25

--- src/app/[locale]/admin/products/create/page.tsx
+++ src/app/[locale]/admin/products/create/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useState, useEffect } from "react";
 import { getCategories, createProduct, uploadImage, getProducts } from "@/lib/api";
 import { Category } from "@/types";
